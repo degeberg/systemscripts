7cebe0de05dddaccebedc3dd57af365f10503190
Daniel Egeberg <daniel.egeberg@gmail.com>
2011-04-30 10:50:09 +0200

    Now using pulseaudio.

--- xmobar_volume.sh
+++ xmobar_volume.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 vol=$(amixer get Master | awk -F'[]%[]' '/%/ { print $2 }' | head -n 1)
-state=$(amixer get Master | awk -F'[]%[]' '/%/ { print $7 }')
+state=$(amixer get Master | awk -F'[]%[]' '/%/ { print $5 }' | head -n 1)
 
 if [ $state == "on" ]; then
     echo "Vol: <fc=green>$vol</fc>%"
